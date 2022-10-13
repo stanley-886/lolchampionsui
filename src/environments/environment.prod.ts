8d58e751ad669eb3394e27c12debcca07b334f8c
stanley-886 <115484212+stanley-886@users.noreply.github.com>
2022-10-13 11:51:19 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "https://lol-champions-ose10-lol.apps.ose.openshift.conygre.com/api/v1/lolchampion"
+  apiUrl: "https://lol-champions-ose4-champions.apps.ose.openshift.conygre.com//api/v1/lolchampion"
 };
